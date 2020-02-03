0f3511110617e896cb6f62c2207808598ae2b367
Amber Volkmann <volkmannamber@gmail.com>
2020-02-03 07:40:20 -0600

    started on edit component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -16,7 +16,7 @@ class Details extends Component {
 
 
   render() {
-    let currentID = this.props.reduxStore.currentMovie
+    // let currentID = this.props.reduxStore.currentMovie
     return (
       <Router>
           <h1>Details</h1>
